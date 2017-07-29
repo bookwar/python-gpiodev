e7f32c978b5fda8c58e2d2712764cf27ca78578a
Aleksandra Fedorova <alpha@bookwar.info>
2017-07-30 00:23:37 +0200

    asyncio first step

--- examples/LCD7.py
+++ examples/LCD7.py
@@ -1,31 +1,47 @@
-from gpiodev.lcd7 import LCD7, render_digit
-import time
+from gpiodev.lcd7 import LCD7
+import asyncio
+
 
 LCD = LCD7(
     (
-        26, #12
-        22, #9
-        17, #8
-        18, #6
+        26,  # 12
+        22,  # 9
+        17,  # 8
+        18,  # 6
     ),
     (
-        19, #11
-        6,  #10
-        4,  #7
-        23, #5
-        21, #1
-        27, #4
-        16, #2
+        19,  # 11
+        6,   # 10
+        4,   # 7
+        23,  # 5
+        21,  # 1
+        27,  # 4
+        16,  # 2
     ),
 )
 
 
-while True:
-    for digit in [
-            (0, "j"),
-            (1, "h"),
-            (2, "k"),
-            (3, "l"),
-    ]:
-        LCD.set_values(render_digit(*digit))
-        time.sleep(0.005)
+async def tick(cycles):
+    for counter in range(cycles):
+        print('tick')
+        for state in [
+                " hal",
+                "hall",
+                "allo",
+                "llo ",
+                "lo h",
+                "o ha",
+        ]:
+            LCD.state = state
+            await asyncio.sleep(0.5)
+    LCD.state = None
+
+
+async def asynchronous():
+    futures = [LCD.async_show("    "), tick(40)]
+    await asyncio.wait(futures)
+
+
+ioloop = asyncio.get_event_loop()
+ioloop.run_until_complete(asynchronous())
+ioloop.close()
--- gpiodev/lcd7.py
+++ gpiodev/lcd7.py
@@ -1,7 +1,5 @@
 from gpiodev import GPIOHandle
-import time
 import asyncio
-
 
 # LCD pins
 
@@ -33,7 +31,7 @@ DIGIT = [
 ]
 
 SYMBOL = {
-    "none": (0, 0, 0, 0, 0, 0, 0),
+    " ": (0, 0, 0, 0, 0, 0, 0),
     "1": (0, 0, 1, 0, 0, 1, 0),
     "a": (1, 1, 1, 1, 1, 1, 0),
     "b": (0, 1, 0, 1, 1, 1, 1),
@@ -42,12 +40,20 @@ SYMBOL = {
     "e": (1, 1, 0, 1, 1, 0, 1),
     "f": (1, 1, 0, 1, 1, 0, 0),
     "g": (1, 1, 0, 0, 1, 1, 1),
-    "h": (0, 1, 0, 1, 1, 1, 0),
+    "h": (0, 1, 1, 1, 1, 1, 0),
     "i": (0, 0, 1, 0, 0, 1, 0),
     "j": (0, 0, 1, 0, 0, 1, 1),
     "k": (0, 1, 1, 1, 1, 1, 0),
     "l": (0, 1, 0, 0, 1, 0, 1),
+    "m": (1, 0, 0, 0, 0, 1, 1),
+    "n": (0, 0, 0, 0, 1, 1, 1),
+    "o": (1, 1, 1, 0, 1, 1, 1),
+    "p": (1, 1, 1, 1, 1, 0, 0),
+    "q": (1, 1, 1, 1, 0, 0, 1),
+    "r": (0, 0, 0, 0, 1, 1, 0),
+    "s": (1, 1, 0, 1, 0, 1, 1),
 }
+
 
 def render_digit(digit, symbol):
     return DIGIT[digit] + SYMBOL[symbol]
@@ -63,8 +69,19 @@ class LCD7(GPIOHandle):
             (1,)*len(self.digits)
             + (0,)*len(self.segments)
         )
-        GPIOHandle.__init__(self,
-                            lines,
-                            defaults=defaults,
-                            mode="out",
+        GPIOHandle.__init__(
+            self,
+            lines,
+            defaults=defaults,
+            mode="out",
         )
+        self.state = None
+
+    async def async_show(self, state=None):
+        if state:
+            self.state = state
+
+        while self.state:
+            for index, symbol in enumerate(self.state):
+                self.set_values(render_digit(index, symbol))
+                await asyncio.sleep(0.001)
